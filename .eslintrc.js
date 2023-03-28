c37f2fe67d6f90e948d6435a99b8305bdb25ec57
mingdama <mma24@dons.usfca.edu>
2023-03-27 23:25:51 -0700

    added style check

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'eqeqeq': 'error',
+    	'no-trailing-spaces': 'error',
+    	'object-curly-spacing': [
+        	'error', 'always'
+    	],
+		'indent': [
+			'error',
+			4
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'no-console': 0
+	}
+}
